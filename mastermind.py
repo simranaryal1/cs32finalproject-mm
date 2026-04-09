85106efd62252f00b80dce4c31cb94d5010532b3
simranaryal1 <simrannp813@gmail.com>
2026-04-09 03:30:44 +0000

    Wed, Apr 8, 2026, 11:30 PM -04:00

---
+++ mastermind.py
@@ -0,0 +1,42 @@
+#mastermind.py
+
+import random
+def compare_guess(secret, guess):
+    full = 0
+    partial = 0
+
+    for i in range (len(secret)):
+        if guess [i] == secret[i]:
+            full +=1
+
+    for ch in guess:
+        if ch in secret:
+            partial +=1
+
+        partial -= full
+
+        return full, partial
+
+def main():
+    print("WELCOME TO MASTERMIND!")
+
+    numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    secret = "".join (random.choice(numbers) for _ in range (4))
+
+    while True:
+        guess = input ("Enter your 4-number guess: ")
+
+        if len(guess) !=4 or not guess.isdigit():
+            print ("Invalid guess, try again")
+            continue
+
+        full, partial = compare_guess (secret, guess)
+
+        print (f"Full: {full}, Partial: {partial}")
+
+        if full ==4:
+            print("You win!")
+            break
+
+if __name__ == "__main__":
+    main()
